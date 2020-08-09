33d1da1d139353918ca223d10b7d4e3c03fa3c69
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-08-09 02:45:06 +0000

    translation of the code into English and changes to README.me

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 # bot-unfollow-instagram-js
-<h3>Português</h3>
-
-Bot (Instagram) para deixar de seguir usuários automaticamente
-
-Modo de uso:
-
-- Encontre as pessoas que você quer deixar de seguir
-- Abra o Inspecionar
-- Vá em Console
-- Cole o código que está dentro do arquivo "index.js" e dê Enter
-
-<h3>Inglês (English)</h3>
 
 Bot (Instagram) to stop following users automatically
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let contador = 0;
+let counter = 0;
 document.querySelectorAll('._8A5w5').forEach((item, index) => {
 setTimeout(() => {
     if(item.innerText == 'Following') {
@@ -6,9 +6,9 @@ setTimeout(() => {
       document.querySelectorAll('.-Cab_').forEach((item, index) => {
         if (item.innerText == 'Unfollow'){
           item.click();
-          contador++;
-          console.log('Você deixou de seguir '+contador+' pessoas');
-          console.log(`Você deixou de seguir ${contador} pessoas`);
+          counter++;
+          console.log('Você deixou de seguir '+counter+' pessoas');
+          console.log(`Você deixou de seguir ${counter} pessoas`);
         }
       })
     }
