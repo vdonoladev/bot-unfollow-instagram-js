de0fcb20063b044d7e5c0a02759a22e808809ebf
vdonoladev <victordonolaf@gmail.com>
2020-10-18 09:39:46 -0300

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let counter = 0;
-document.querySelectorAll('._8A5w5').forEach((item, index) => {
-setTimeout(() => {
-    if(item.innerText == 'Seguindo') {
+document.querySelectorAll("._8A5w5").forEach((item, index) => {
+  setTimeout(() => {
+    if (item.innerText == "Seguindo") {
       item.click();
-      document.querySelectorAll('.-Cab_').forEach((item, index) => {
-        if (item.innerText == 'Deixar de seguir'){
+      document.querySelectorAll(".-Cab_").forEach((item, index) => {
+        if (item.innerText == "Deixar de seguir") {
           item.click();
           counter++;
-          console.log('Você deixou de seguir '+counter+' pessoas');
+          console.log("Você deixou de seguir " + counter + " pessoas");
           console.log(`Você deixou de seguir ${counter} pessoas`);
         }
-      })
+      });
     }
-  }, index * 10000)
-})
+  }, index * 10000);
+});
